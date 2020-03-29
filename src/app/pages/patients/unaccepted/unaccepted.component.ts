a97df8128f0cbbcea82cfaa202f82dbcedcd74f0
Stepan Skopivskiy <stepan@skop.in.ua>
2020-03-29 19:19:38 +0300

    Added unaccepted page

---
+++ src/app/pages/patients/unaccepted/unaccepted.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {PatientsService} from "../../../core/patients.service";
+
+@Component({
+  selector: 'app-unaccepted',
+  templateUrl: './unaccepted.component.html',
+  styleUrls: ['./unaccepted.component.scss']
+})
+export class UnacceptedComponent implements OnInit {
+
+  public $list;
+  public displayedColumns: string[] = [
+    'id',
+    'age',
+    'diagnose_first_date',
+    'last_replacement_therapy_end_date',
+    'last_replacement_therapy_injection_type',
+    'last_replacement_therapy_dose',
+    'last_replacement_therapy_producer'
+  ];
+
+  constructor(private patientService: PatientsService) {
+  }
+
+  ngOnInit(): void {
+    this.$list = this.patientService.unaccepted();
+  }
+
+}
